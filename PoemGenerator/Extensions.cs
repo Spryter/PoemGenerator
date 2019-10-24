037909a0ce2be786d43fe13d2e8e587bd2e192b8
Spryt <zxcz@list.ru>
2019-10-25 01:17:25 +0500

    Добавьте файлы проекта.

---
+++ PoemGenerator/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoemGenerator
+{
+	public static class jsonExtensions
+	{
+		public static IEnumerable<JToken> GetNodesByForwardRelationName(this JObject ontology, JToken baseNode, string relationName)
+		{
+			var nodes = ((JArray)ontology["nodes"]).Children();
+			var relations = ((JArray)ontology["relations"]).Children();
+			var relation = relations.Where(r => r["source_node_id"].ToString() == baseNode["id"].ToString() && r["name"].ToString() == relationName).FirstOrDefault();
+			var needRelationDestinations = relations.Where(r => r["source_node_id"].ToString() == baseNode["id"].ToString() && r["name"].ToString() == relationName).Select(r => r["destination_node_id"].ToString());
+			return nodes.Where(n => needRelationDestinations.Contains(n["id"].ToString()));
+		}
+		public static IEnumerable<JToken> GetNodesByBackwardRelationName(this JObject ontology, JToken baseNode, string relationName)
+		{
+			var nodes = ((JArray)ontology["nodes"]).Children();
+			var relations = ((JArray)ontology["relations"]).Children();
+			var relation = relations.Where(r => r["source_node_id"].ToString() == baseNode["id"].ToString() && r["name"].ToString() == relationName).FirstOrDefault();
+			var needRelationDestinations = relations.Where(r => r["destination_node_id"].ToString() == baseNode["id"].ToString() && r["name"].ToString() == relationName).Select(r => r["source_node_id"].ToString());
+			return nodes.Where(n => needRelationDestinations.Contains(n["id"].ToString()));
+		}
+	}
+}
---
+++ PoemGenerator/MainForm.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PoemGenerator
+{
+	public partial class MainForm : Form
+	{
+		JObject ontology;
+		JEnumerable<JToken> nodes;
+		JEnumerable<JToken> relations;
+		public MainForm()
+		{
+			InitializeComponent();
+			ontology = JObject.Parse(File.ReadAllText(@"C:\Users\Fox\source\repos\PoemGenerator\PoemGenerator\black_poems.ont"));
+			nodes = ((JArray)ontology["nodes"]).Children();
+			relations = ((JArray)ontology["relations"]).Children();
+		}
+
+		private void btGenerate_Click(object sender, EventArgs e)
+		{
+			var poemNode = nodes.Where(n => n["name"].ToString() == "стихотворение").FirstOrDefault();
+			var strings = ontology.GetNodesByBackwardRelationName(poemNode, "a_part_of");
+			var string1 = strings.Where(s => s["name"].ToString() == "1 строка").FirstOrDefault();
+			var tokens = ontology.GetNodesByBackwardRelationName(string1, "a_part_of");
+			var token1 = tokens.Where(t => ontology.GetNodesByForwardRelationName(t, "order").FirstOrDefault()["name"].ToString() == "1").FirstOrDefault();
+			var elementOfToken1 = ontology.GetNodesByForwardRelationName(token1, "element").FirstOrDefault();
+			var concreteElements = ontology.GetNodesByBackwardRelationName(elementOfToken1, "is_a");
+			Random rnd = new Random();
+			int index = rnd.Next(concreteElements.Count());
+			var concreteElement = concreteElements.ElementAt(index);
+			rtbPoem.Text = concreteElement["name"].ToString();
+		}
+	}
+}
